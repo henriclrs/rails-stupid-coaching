7753f5e562ebcfe93e57bb4a0b4f79804c9a401f
henri clérisse <henri.clerisse@gmail.com>
2018-11-26 13:23:09 +0100

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+
+    if @question == 'I am going to work'
+      return @answer = 'great!'
+    elsif @question.include? '?'
+      return @answer = 'Silly question, get dressed and go to work!'
+    else
+      return @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
